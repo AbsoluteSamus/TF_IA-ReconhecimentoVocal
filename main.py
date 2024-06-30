b59ffa23bab3b21c9b990234402acffd94b4814d
Samuel <samuelbisneto10@gmail.com>
2024-06-30 20:27:59 -0300

    new formula

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
+import tkinter as tk
+from tkinter import messagebox
 from Twilio_Whats import enviar_mensagem_whatsapp
 from Reconhecimento import reconhecer_audio
 from train import censurar_frase
 
-def main():
-    # Configuração das credenciais do Twilio
-    account_sid = 'AC64c7ec8bb74c0d57612b17238b92d846'
-    auth_token = '9c77c9cdaf6c65351d9846960a6a0016'
-    from_number = 'whatsapp:+14155238886'  # Número do sandbox do Twilio
-    to_number = 'whatsapp:+558694859177'  # Número de destino com o código do país
-
-    # Chama a função para capturar áudio e reconhecer o texto
+def processar_frase():
     texto_reconhecido = reconhecer_audio()
-
-    # Censura a frase reconhecida
+    
     if texto_reconhecido:
         texto_censurado = censurar_frase(texto_reconhecido)
-        print(f"Texto censurado: {texto_censurado}")
+        messagebox.showinfo("Texto Reconhecido e Censurado", f"Texto censurado: {texto_censurado}")
+        
+        # Configuração das credenciais do Twilio
+        account_sid = 'AC64c7ec8bb74c0d57612b17238b92d846'
+        auth_token = '3ac98e118c95dbc72353d2e581bdb372'
+        from_number = 'whatsapp:+14155238886'  # Número do sandbox do Twilio
+        to_number = 'whatsapp:+558694859177'  # Número de destino com o código do país
+        
+        try:
+            mensagem_enviada = enviar_mensagem_whatsapp(account_sid, auth_token, from_number, to_number, texto_censurado)
+            messagebox.showinfo("Mensagem Enviada", f"Mensagem enviada com sucesso: {mensagem_enviada}")
+        except Exception as e:
+            messagebox.showerror("Erro ao Enviar Mensagem", f"Erro: {str(e)}")
 
-        # Envio da mensagem censurada pelo WhatsApp usando Twilio
-        mensagem = texto_censurado
-        mensagem_enviada = enviar_mensagem_whatsapp(account_sid, auth_token, from_number, to_number, mensagem)
-        print(f"Mensagem enviada com sucesso: {mensagem_enviada}")
+# Criando a janela principal
+root = tk.Tk()
+root.title("Reconhecimento de Voz e Envio pelo WhatsApp")
 
-if __name__ == "__main__":
-    main()
+# Criando e posicionando os widgets
+label = tk.Label(root, text="Clique no botão para iniciar o reconhecimento de voz e envio pelo WhatsApp:")
+label.pack(pady=10)
+
+botao = tk.Button(root, text="Iniciar Reconhecimento e Envio", command=processar_frase)
+botao.pack(pady=10)
+
+# Loop principal da interface gráfica
+root.mainloop()
